f1a7f144a2c5ddd4781b97cbee1d6579e7007c0c
AlejandroSampablo <alejandro200304@gmail.com>
2024-04-04 15:34:22 -0600

    Segundo Commit

--- src/reducers/activityReducer.ts
+++ src/reducers/activityReducer.ts
@@ -1,25 +1,25 @@
-import { Activity } from "../types";
+import { Activity } from "../types"
 
 export type ActivityActions =
    { type: "save-activity", payload: { newActivity: Activity } }
   | { type: "set-activeId", payload: { id: Activity["id"] } }
   | { type: "delete-activity", payload: { id: Activity["id"] } }
-  | { type: 'restart-app'}
+  | { type: "restart-app"}
 
 export type ActivityState = {
   activities: Activity[],
   activeId: Activity["id"]
 };
 
-const localStorageActivities =(): Activity[]=>{
-  const activities = localStorage.getItem("activities");
-  return activities ? JSON.parse(activities) : [];  
+const localStorageActivities = () : Activity[] => {
+    const activities = localStorage.getItem('activities') 
+    return activities ? JSON.parse(activities) : []
 }
 
-export const initialState: ActivityState = {
-  activities: localStorageActivities(),
-  activeId: ''
-};
+export const initialState : ActivityState = {
+    activities: localStorageActivities(),
+    activeId: ""
+}
 
 export const activityReducer = (
   state: ActivityState = initialState,
@@ -30,22 +30,22 @@ export const activityReducer = (
     if (state.activeId) {
       updatedActivities = state.activities.map((activity) =>
         activity.id === state.activeId ? action.payload.newActivity : activity
-      );
+      )
     } else {
       updatedActivities = [...state.activities, action.payload.newActivity];
     }
     return {
       ...state,
       activities: updatedActivities,
-      activeId: "",
-    };
+      activeId: ""
+    }
   }
-  if (action.type === "set-activeId") {
-    return {
-      ...state,
-      activeId: action.payload.id,
-    };
-  }
+   if(action.type === 'set-activeId') {
+         return {
+            ...state,
+            activeId: action.payload.id
+         }
+    }
   if (action.type === "delete-activity") {
     return {
       ...state,
@@ -54,10 +54,10 @@ export const activityReducer = (
       ),
     };
   }
-  if(action.type === "restart-app"){
+  if(action.type === 'restart-app'){
     return{
       activities: [],
-      activeid: ''
+      activeId: ''
     }
   }
   return state
